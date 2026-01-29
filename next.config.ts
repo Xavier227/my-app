03cabcfb2f98a89e7b3b3eb39643a4ab5ecdb7d0
Xavier <xavierdamours@gmail.com>
2026-01-28 20:39:53 -0500

    ajout deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/my-app", // your GitHub repository name
+  assetPrefix: "/my-app/", // ensures CSS and JS links work
 };
 
 export default nextConfig;
