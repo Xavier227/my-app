47c1682d48a5a007b0a71d096726307a541c2cea
Xavier <xavierdamours@gmail.com>
2026-01-29 16:23:34 -0500

    add ROOT_PATH

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/my-app", // your GitHub repository name
+  basePath: process.env.ROOT_PATH || "/my-app", // your GitHub repository name
   assetPrefix: "/my-app/", // ensures CSS and JS links work
 };
 
