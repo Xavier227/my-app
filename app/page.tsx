1869b7e13ed153f2d602c35f6593821946f1e3a3
Xavier <xavierdamours@gmail.com>
2026-01-31 22:24:57 -0500

    ajout toast

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,68 @@ import { useState } from "react";
 
 import Toolbar from "@/components/Toolbar";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarGroup,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { FileIcon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { toast } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function Page() {
   const [showMessage, setShowMessage] = useState(false);
 
   return (
     <div className=" min-h-screen flex flex-col bg-gray-900 ">
-      <Toolbar />
+      <Toaster />
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarGroup>
+              <MenubarItem>
+                <FileIcon />
+                New Tab <MenubarShortcut>Ctrl+T</MenubarShortcut>
+              </MenubarItem>
+            </MenubarGroup>
+            <MenubarSeparator />
+            <MenubarGroup>
+              <MenubarItem>Share</MenubarItem>
+              <MenubarItem>Print</MenubarItem>
+              <MenubarItem></MenubarItem>
+            </MenubarGroup>
+          </MenubarContent>
+        </MenubarMenu>
+        <MenubarMenu>
+          <MenubarTrigger>Edit</MenubarTrigger>
+          <MenubarContent>
+            <MenubarGroup>
+              <MenubarItem>
+                New Tab <MenubarShortcut>Ctrl+T</MenubarShortcut>
+              </MenubarItem>
+            </MenubarGroup>
+            <MenubarSeparator />
+            <MenubarGroup>
+              <MenubarItem>Share</MenubarItem>
+              <MenubarItem>Print</MenubarItem>
+            </MenubarGroup>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
       <main className="p-4 flex flex-col items-center text-white">
         <h1 className="text-2xl font-bold">Home Page</h1>
         <p>Welcome to the home page!</p>
+        <Skeleton className="h-[20px] w-[100px] rounded-full" />
         <div className="grid grid-cols-3 place-items-center mt-5 gap-y-2 gap-x-10 ">
           <p>test1</p>
           <p>test2</p>
@@ -36,15 +88,32 @@ export default function Page() {
           <p>test2</p>
           <p>test3</p>
         </div>
+        <Field className="max-w-md">
+          <FieldLabel htmlFor="email">Your email address</FieldLabel>
+          <Input id="email" />
+        </Field>
         <div className="flex justify-center items-center mt-8">
-          <Button onClick={() => setShowMessage(true)} variant="outline">
+          <Button
+            onClick={() => setShowMessage(true)}
+            variant="outline"
+            className="text-black"
+          >
             Show Popup
+          </Button>
+          <Button
+            onClick={() =>
+              toast.info("Test toast", { position: "top-center" })
+            }
+            variant="outline"
+            className="text-black"
+          >
+            Show Toast
           </Button>
         </div>
         {/* Popup box */}
         {showMessage && (
           <div className="fixed inset-0 flex items-center justify-center z-10">
-            <div className="bg-gray-800 text-white rounded-3xl shadow-lg p-8 flex flex-col items-center">
+            <div className="bg-gray-800 text-white rounded-3xl shadow-xl p-8 flex flex-col items-center">
               <p className="text-center mb-4">This is your popup message!</p>
               <Button
                 onClick={() => setShowMessage(false)}
